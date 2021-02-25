273fb631c87ccff64bc0f7da31a8a334f2e5acc3
Datila Silva <datilaadv@gmail.com>
2021-02-25 10:14:05 +0100

    add README and JS

---
+++ script1.js
@@ -0,0 +1,10 @@
+/*Trilha 01 - Trecho 01 - Noções de Computação (Warm-up) Intervalo 1 - Soma de dois números
+Apresente um pseudocódigo (em pdf ou doc) para solucionar computacionalmente o seguinte problema:
+Pedro e João precisam realizar a soma de dois números e apresentar para a professora o resultado. 
+Faça um algoritmo que leia os dois números e imprima o resultado da soma destes.*/
+
+
+    var numeroJoao = 2;
+    var numeroPedro = 5;
+    var results = numeroJoao + numeroPedro;
+    console.log('A soma dos 2 números apresentados por João e Pedro é:', results) 
